89ab69313d6239b217d14cac183ea299e8c96fb2
Sampath Reddy Ette <sampathreddy1201@gmail.com>
2024-07-02 23:00:54 -0700

    commented iam code

--- infra/main.tf
+++ infra/main.tf
@@ -55,19 +55,19 @@ module "create_pubsub_topic" {
   retention_time    = "86600s"
 }
 
-module "create-sa" {
-  source          = "../modules/sa"
-  for_each        = var.service_accounts
-  project_id      = var.project_name
-  display_name    = each.value.display_name
-  account_id_name = each.value.name
-}
-
-# module "create-dataproc" {
-#   source              = "../modules/dataproc"
-#   project_id          = var.project_name
-#   cluster_name        = "dataproc-test-composer"
-#   location            = var.region
-#   account_id          = var.service_accounts["dataproc_service_account"].name
-#   staging_bucket_name = "a5d7dd6899e4c0ee-bucket-tfstate"
+# module "create-sa" {
+#   source          = "../modules/sa"
+#   for_each        = var.service_accounts
+#   project_id      = var.project_name
+#   display_name    = each.value.display_name
+#   account_id_name = each.value.name
 # }
+
+module "create-dataproc" {
+  source              = "../modules/dataproc"
+  project_id          = var.project_name
+  cluster_name        = "dataproc-test-composer"
+  location            = var.region
+  account_id          = var.service_accounts["dataproc_service_account"].name
+  staging_bucket_name = "a5d7dd6899e4c0ee-bucket-tfstate"
+}
